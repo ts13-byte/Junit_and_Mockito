55c947c83ef1556aa8ed284d5dc7f23534e73aeb
Sara Tripathi <saratripathi0924@gmail.com>
2023-01-10 21:48:32 +0530

    some additions

---
+++ Test/com/Sara/test/MyAssertTest.java
@@ -0,0 +1,25 @@
+package com.Sara.test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class MyAssertTest {
+
+	List<String> ls= Arrays.asList("AWS","Java","Python","C++");
+	@Test
+	void Test() {
+		boolean test=ls.contains("AWS");
+		assertEquals(true,test);
+		assertTrue(test);
+		//assertFalse(test);
+		assertArrayEquals(new int[] {1,2}, new int[] {1,2});
+		assertEquals(4,ls.size());
+		
+	}
+
+}
